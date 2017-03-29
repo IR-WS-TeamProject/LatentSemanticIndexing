855169700859235a4e9f3acbb20b2d88b542174d
alexandrahofmann <alexandra_hofmann@gmx.de>
2017-03-29 17:13:44 +0200

    include ascii special characters in bagofwords

--- PythonModule/files/Preprocessing/FilePreprocessing.py
+++ PythonModule/files/Preprocessing/FilePreprocessing.py
@@ -48,26 +48,58 @@ def getBagOfWords(inputString):
 
     # TODO: how to handle special characters etc. ?!?!
 
-
     #------------------------------------------------------------------
 
     # very cumbersome and not performant, but one way would be:
     returnString = ''.join(i for i in returnString if ord(i) < 128) # this limits the allowed characters to ASCI II
 
+    # exclude header parameters
+    returnString = returnString.replace("from:", "")
+    returnString = returnString.replace("subject:", "")
+    returnString = returnString.replace("re:", "")
+    returnString = returnString.replace("organization:", "")
+    returnString = returnString.replace("distribution:", "")
+    returnString = returnString.replace("lines:", "")
+    returnString = returnString.replace("reply-to:", "")
+
+
     # take care of special characters...
-    returnString = returnString.replace(": ", " ") # allows for tokens like "12:00:30"
-    returnString = returnString.replace(". ", " ") # allows for tokens like "10.50" or dates like "2017.03.05"
-    returnString = returnString.replace(", ", " ")
-    returnString = returnString.replace("(", "")
-    returnString = returnString.replace(")", "")
-    returnString = returnString.replace("$", "")
-    returnString = returnString.replace("]", "")
-    returnString = returnString.replace("[", "")
-    returnString = returnString.replace(", ", " ") # allows for tokens like 5,5 (German decimal)
-    returnString = returnString.replace(";", "")
-    returnString = returnString.replace("-", "")
-    returnString = returnString.replace(">", "")
-    returnString = returnString.replace("<", "")
+    returnString = returnString.replace(": ", " ")  # allows for tokens like "12:00:30"
+    returnString = returnString.replace(". ", " ")  # allows for tokens like "10.50" or dates like "2017.03.05"
+    returnString = returnString.replace(", ", " ")  # allows for tokens like 5,5 (German decimal)
+
+    returnString = returnString.replace("", "")     # ascii 32: space
+    returnString = returnString.replace("!", "")    # ascii 33
+    returnString = returnString.replace('\"', "")   # ascii 34: quotation marks
+    returnString = returnString.replace("#", "")    # ascii 35
+    returnString = returnString.replace("$", "")    # ascii 36
+    returnString = returnString.replace("%", "")    # ascii 37
+    returnString = returnString.replace("&", "")    # ascii 38
+    returnString = returnString.replace("\'", "")   # ascii 39: single quote
+    returnString = returnString.replace("(", "")    # ascii 40
+    returnString = returnString.replace(")", "")    # ascii 41
+    returnString = returnString.replace("*", "")    # ascii 42
+    returnString = returnString.replace("+", "")    # ascii 43
+    returnString = returnString.replace("-", "")    # ascii 45
+    returnString = returnString.replace("/", "")   # ascii 47
+
+    returnString = returnString.replace(";", "")    # ascii 59
+    returnString = returnString.replace("<", "")    # ascii 60
+    returnString = returnString.replace("=", "")    # ascii 61
+    returnString = returnString.replace(">", "")    # ascii 62
+    returnString = returnString.replace("?", "")    # ascii 63
+
+    returnString = returnString.replace("[", "")    # ascii 91
+    returnString = returnString.replace("\\", "")   # ascii 92
+    returnString = returnString.replace("]", "")    # ascii 93
+    returnString = returnString.replace("", "")     # ascii 94
+    #returnString = returnString.replace("_", "")   # ascii 95
+
+    returnString = returnString.replace("{", "")    # ascii 123
+    returnString = returnString.replace("|", "")    # ascii 124
+    returnString = returnString.replace("}", "")    # ascii 125
+    returnString = returnString.replace("~", "")    # ascii 126
+
     # TODO: Cover all ASCII special characters (not that many)
 
     #------------------------------------------------------------------
@@ -85,10 +117,12 @@ def getBagOfWords(inputString):
     return returnString
 
 
+
+
 # example of how to call the method
-myRootDirectory = "C:/Users/D060249/Documents/Mannheim/Semester 2/Information Retrieval and Web Search/IR Team Project/20news-bydate-train"
+myRootDirectory = "/Users/alexandrahofmann/Documents/Master Uni MA/2. Semester/Information Retrieval and Web Search/Team Project/20news-bydate-train"
 allFiles = getPathsToAllResourceFiles(myRootDirectory)
-print (allFiles) # print all files
+# print (allFiles) # print all files
 
 # Read the first file as a single string and output
 with open(allFiles[0], 'rt') as f:

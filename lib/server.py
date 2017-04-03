97d420e89012c6633cbc32190a04a7a6b7c5a908
David Hentschel <David Hentschel>
2017-04-03 16:59:54 +0200

    prettify code using pylint

--- lib/server.py
+++ lib/server.py
@@ -1,67 +1,73 @@
-import http.server
-import socketserver
-from os import curdir, sep
+"""
+	This module provides a server, which can deliver a webapp
+	and trigger the search algorithm for an arbitrary query
+	(based on the latent semantic indexing method).
+"""
+
+#from os import curdir, sep
 import urllib
 import json
-
+import http.server
+import socketserver
 import queryexec
-
-#https://docs.python.org/3/library/http.server.html
 
 PORT = 8000
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
-	def do_HEAD(s):
-		s.send_response(200)
-		s.send_header("Content-type", "text/html")
-		s.end_headers()
-	def do_GET(self):
-		print("self.path", self.path)
-		if self.path == '/':
-			path = './ui/dist/index.html'
-			contenttype = 'text/html'
-			print("path: ", path)
-			f = f = open(path, 'rb')
-			self.send_response(200)
-			self.send_header('Content-type',contenttype)
-			self.end_headers()
-			self.wfile.write(f.read())
-			f.close()
-		elif self.path.startswith('/api?query='):
-			query = self.path.replace('/api?query=', '')
-			query = urllib.parse.unquote(query)
-			print('query: ', query)
-			## TODO resolve mock
-			rankedList = queryexec.getRankedListMock()
-			##
-			contenttype = 'application/json'
-			body = json.dumps(rankedList)
-			self.send_response(200)
-			self.send_header('Content-type',contenttype)
-			self.end_headers()
-			self.wfile.write(bytearray(body, 'utf-8'))
-		elif self.path.startswith('/api?doc='):
-			doc = self.path.replace('/api?doc=', '')
-			path = './data/20news-bydate/20news-bydate-train/' + doc
-			contenttype = 'text/plain'
-			print("path: ", path)
-			f = f = open(path, 'rb')
-			self.send_response(200)
-			self.send_header('Content-type',contenttype)
-			self.end_headers()
-			self.wfile.write(f.read())
-			f.close()
-		else:
-			path = './ui/dist' + self.path
-			contenttype = 'application/javascript'
-			print("path: ", path)
-			f = f = open(path, 'rb')
-			self.send_response(200)
-			self.send_header('Content-type',contenttype)
-			self.end_headers()
-			self.wfile.write(f.read())
-			f.close()
-		return
+    """Request Handler for the server"""
+    def do_HEAD(self):
+        """Check if server is running"""
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+    def do_GET(self):
+        """Get webapp, documents and request api to retrieve ranked list"""
+        print("self.path", self.path)
+        if self.path == '/':
+            path = './ui/dist/index.html'
+            contenttype = 'text/html'
+            print("path: ", path)
+            file = open(path, 'rb')
+            self.send_response(200)
+            self.send_header('Content-type', contenttype)
+            self.end_headers()
+            self.wfile.write(file.read())
+            file.close()
+        elif self.path.startswith('/api?query='):
+            query = self.path.replace('/api?query=', '')
+            query = urllib.parse.unquote(query)
+            print('query: ', query)
+            ## TODO resolve mock
+            ranked_list = queryexec.get_ranked_list_mock()
+            ##
+            contenttype = 'application/json'
+            body = json.dumps(ranked_list)
+            self.send_response(200)
+            self.send_header('Content-type', contenttype)
+            self.end_headers()
+            self.wfile.write(bytearray(body, 'utf-8'))
+        elif self.path.startswith('/api?doc='):
+            doc = self.path.replace('/api?doc=', '')
+            path = './data/20news-bydate/20news-bydate-train/' + doc
+            contenttype = 'text/plain'
+            print("path: ", path)
+            file = open(path, 'rb')
+            self.send_response(200)
+            self.send_header('Content-type', contenttype)
+            self.end_headers()
+            self.wfile.write(file.read())
+            file.close()
+        else:
+            path = './ui/dist' + self.path
+            contenttype = 'application/javascript'
+            print("path: ", path)
+            file = open(path, 'rb')
+            self.send_response(200)
+            self.send_header('Content-type', contenttype)
+            self.end_headers()
+            self.wfile.write(file.read())
+            file.close()
+        return
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
     print("serving at port", PORT)

bf7130fd2f4fa896f3f64b0bcd07113db57c991d
Sascha Ulbrich <saschatu@gmx.de>
2017-04-04 14:04:30 +0200

    test

--- lib/testing.py
+++ lib/testing.py
@@ -13,18 +13,18 @@ svdHandler = svd.SVDHandler(tdm=tfidfHandler.getTDM(), docs=tfidfHandler.getDocs
 query = "MIT Lincoln LaboratoryDistribution: source of orbital element sets other than UAF/Space Command. I believe there is one on CompuServe." # Doc sci.space/60158
 
 #Convert into term vector
-termVec = fp.stringTransformation(query)
-#termVec  = ['underground','underwater','wireless','communications'] #Doc sci.electronics/52434
-print("Test TermVec:", termVec)
+#bow = fp.stringTransformation(query)
+bow  = ['underground','underwater','wireless','communications'] #Doc sci.electronics/52434
+print("Test TermVec:", bow)
 
 #convert into topic vector (SVD)
-topicVec = tfidfHandler.convertDocToVec(termVec)
-print("TopicVec, Non-Zero pos:", topicVec.nonzero())
-print("TopicVec, Non-Zero values:", topicVec[topicVec.nonzero()])
-print("TopicVec, size:", topicVec.size)
+termVec = tfidfHandler.convertDocToVec(bow)
+print("TopicVec, Non-Zero pos:", termVec.nonzero())
+print("TopicVec, Non-Zero values:", termVec[termVec.nonzero()])
+print("TopicVec, size:", termVec.size)
 
 #Calc. LSI ranking
-docs, sims = svdHandler.getRanking(topicVec, n = 10)
+docs, sims = svdHandler.getRanking(termVec, n = 10)
 print("Top-10 Docs:", docs )
 print("Similarities:", sims)
 
